af19726682256ec10485b5b069115777bad54c8d
Carlton Whitehead <cebesius86@gmail.com>
2017-11-26 18:05:15 -0500

    Initial test for the LogoView

--- src/test/kotlin/org/coner/worker/view/LogoViewTest.kt
+++ src/test/kotlin/org/coner/worker/view/LogoViewTest.kt
@@ -4,31 +4,28 @@ import javafx.scene.Scene
 import javafx.stage.Stage
 import org.junit.Test
 import org.testfx.api.FxAssert
-import org.testfx.api.FxToolkit
 import org.testfx.framework.junit.ApplicationTest
+import org.testfx.matcher.base.NodeMatchers
 import org.testfx.matcher.control.TextMatchers
 
 class LogoViewTest : ApplicationTest() {
 
-    override fun init() {
-        super.init()
-        FxToolkit.registerStage { Stage() }
-    }
+    private lateinit var logoView: LogoView
 
     override fun start(stage: Stage) {
         super.start(stage)
-        val logoView = LogoView()
+        logoView = LogoView()
         stage.scene = Scene(logoView.root)
         stage.show()
     }
 
-    override fun stop() {
-        super.stop()
-        FxToolkit.hideStage()
+    @Test
+    fun shouldContainLogo() {
+        FxAssert.verifyThat("#logo", NodeMatchers.isNotNull())
     }
 
     @Test
     fun shouldContainWorkerText() {
-        FxAssert.verifyThat(".text", TextMatchers.hasText("Worker"))
+        FxAssert.verifyThat("#title_short", TextMatchers.hasText("Worker"))
     }
 }
